fe70629e2f73c3c6064dd8da623b8b07d4e1ca73
AngelLM <alm37454@gmail.com>
2018-01-17 19:43:28 +0100

    GCodeTrimmer!

---
+++ GcodeTrimmer.py
@@ -0,0 +1,63 @@
+import sys
+import time
+
+if len(sys.argv)<4:
+    sys.exit("Not enough arguments. Please check it.")
+if len(sys.argv)>4:
+    sys.exit("Too many arguments. Please check it.")
+if not sys.argv[1].replace(".", "", 1).isdigit():
+    sys.exit("The first argument does not contain only a digits. Check it.")
+if sys.argv[2].find('.gcode')==-1:
+    sys.exit("Not .gcode file extension found on origin file argument. Check it.")
+if sys.argv[3].find('.gcode')==-1:
+    sys.exit("Not .gcode file extension found on destination file argument. Check it.")
+
+start_time = time.time()
+
+FirstZMoveString = "G1 Z15.0 F1200          ;move the platform down 15mm"
+EndOfStartGcodeString = "M106 S255"
+ZeroExtrudeLenghtString = "G92 E0                  ;zero the extruded length again"
+
+Zstring = "Z"+ str(sys.argv[1])
+ZmoveFirst = "Z"+ str((float(sys.argv[1])+5))
+
+try:
+    fread = open(sys.argv[2], 'r')
+
+except:
+    sys.exit("Origin gcode file not found. Check it.")
+
+originalgcode = (fread.read())
+fread.close()
+
+newsplitted=originalgcode.split("\n")
+matching = [s for s in newsplitted if Zstring in s]
+indexmatch = newsplitted.index(matching[0])
+
+Xmatch = newsplitted[indexmatch].split(" ")[1]
+Ymatch = newsplitted[indexmatch].split(" ")[2]
+Zmatch = newsplitted[indexmatch].split(" ")[3]
+
+indexFirstZMove = newsplitted.index(FirstZMoveString)
+FirstZMoveStringSplitted=FirstZMoveString.split(" ")
+newsplitted[indexFirstZMove]=FirstZMoveStringSplitted[0]+" "+ZmoveFirst+" "+FirstZMoveStringSplitted[2]
+
+indexEndStartGcode = newsplitted.index(EndOfStartGcodeString)
+
+indexResetExtrude = newsplitted.index(ZeroExtrudeLenghtString)
+for i in range(1,10):
+    if newsplitted[indexmatch-i].find(' E')!=-1:
+        Ematch=newsplitted[indexmatch-i].split(" ")[3]
+        newsplitted[indexResetExtrude]= "G92 "+Ematch
+        break
+
+for x in reversed(range(indexEndStartGcode+1, indexmatch)):
+    newsplitted.pop(x)
+
+newJoined='\n'.join(newsplitted)
+
+fwrite = open(sys.argv[3], 'w+')
+fwrite.write(newJoined)
+fwrite.close()
+
+print ("Done in " + str(time.time()-start_time) + " seconds!")
--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # GcodeTrimmer
+
+Sometimes when 3D printing the printer might run out of filament, it might break or get stuck. Then you have 2 options: to try to resume the print where it failed or restart it from scratch.
+
+This script allows you to trim the Gcode easily at the height of your choice. It has been tested with a GCode made by Cura and it worked (example files are available in the "examples" folder).
+
+Feel free to adapt it to your needs and share the modifications!
+
+## How to resume a failed print.
+
+Note: depending on the level of failure some prints couldn't be resumed properly.
+
+1. Scrape the surface of your print until you get a flat surface (that should be the last well printed layer)
+2. Measure the height of the print (the use of a caliper is suggested)
+3. Open the gcode file of the failed print in the PC (text editor/notepad can do the work)
+4. Find the closer Z height to your measure and remember it. (You can use the find tool to search "Z")
+5. Execute the script using the Z height found in the previous step
+6. Print the generated Gcode
+7. Cross your fingers
+
+
+## How to use the script
+
+1. Clone the repository (or just download the python script).
+2. Open the terminal and navigate to the folder where the script is.
+3. Execute the script using python passing 3 additional arguments (Z coordinate, path to original gcode file, path to the new generated gcode file)
+
+For example:
+```
+python GcodeTrimmer.py 14.200 examples/original.gcode examples/destination.gcode
+```
+
+Executing the script in this way, will generate a new file (destination.gcode) in the examples folder that will be the original file (original.gcode) trimmed at Z14.200
+
+
+## License
+
+<img src="By-sa.png" width="200">
+
+All files included in this repository are licensed under a [Creative Commons Attribution-ShareAlike 4.0 International License](http://creativecommons.org/licenses/by-sa/4.0/)
